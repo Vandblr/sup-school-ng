c4081fc8f613203671f5412084aab2ba2efe11f9
Vanderlei Bandeira <vanderlei123bandeira4@gmail.com>
2025-09-22 21:53:07 -0300

    aulas-22-09-25

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,8 @@ import { CalculadoraRetangulo } from './components/calculadora-retangulo/calcula
 import { CalculadoraMedia } from './components/calculadora-media/calculadora-media';
 import { CalculadoraTemperatura } from './components/calculadora-temperatura/calculadora-temperatura';
 import { ListaTarefas } from './components/lista-tarefas/lista-tarefas';
+import { ListaAlunos } from './components/lista-alunos/lista-alunos';
+import { CadastroAluno } from './components/cadastro-aluno/cadastro-aluno';
 
 export const routes: Routes = [
     {path: "calculadora", component: Calculadora},
@@ -13,5 +15,7 @@ export const routes: Routes = [
     {path: "calculadora-media", component: CalculadoraMedia},
     {path: "calculadora-temperatura", component: CalculadoraTemperatura},
     {path: "lista-tarefas", component: ListaTarefas},
+    { path: "alunos", component: ListaAlunos },
+    { path: "alunos/cadastro", component: CadastroAluno}
     
 ];
