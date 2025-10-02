6e3a5c25579167feab3c809eb735d66ee1a174e0
Vanderlei Bandeira <vanderlei123bandeira4@gmail.com>
2025-10-01 22:03:03 -0300

    projeto com primeNg

---
+++ biblioteca-ng/src/app/components/navbar/navbar.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { Menubar } from 'primeng/menubar';
+
+@Component({
+  selector: 'navbar',
+  imports: [Menubar],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.scss'
+})
+export class Navbar {
+  items: MenuItem[] | undefined;
+
+    ngOnInit() {
+        this.items = [
+            {
+                label: 'Inicio',
+                icon: 'pi pi-home',
+                routerLink: "/"
+            },
+            {
+                label: 'Cadastros',
+                icon: 'pi pi-cog',
+                items: [
+                    {
+                        label: 'Autores',
+                        icon: 'pi pi-users'
+                    },
+                    {
+                        label: 'Categorias',
+                        icon: 'pi pi-list',
+                        routerLink:"categorias"
+                    },
+                    {
+                        label: 'Livros',
+                        icon: 'pi pi-book'
+                    },
+                    {
+                        label: 'Usuários',
+                        icon: 'pi pi-user'
+                    },
+                ]
+            },
+        ]
+    }
+}
+
+
---
+++ biblioteca-ng/src/app/pages/categorias/list/list.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { Button, ButtonModule } from 'primeng/button';
+import { CategoriaResponse } from '../../../models/categoria.dto';
+import { TableModule } from 'primeng/table';
+import { CommonModule } from '@angular/common';
+import { CategoriaService } from '../../../services/categoria.service';
+
+@Component({
+  selector: 'categoria-list',
+  imports: [ButtonModule, TableModule, CommonModule],
+  templateUrl: './list.html',
+  styleUrl: './list.scss'
+})
+export class CategoriaList {
+  categorias: CategoriaResponse[] = [];
+
+  constructor(private categoriaService: CategoriaService){
+
+  }
+
+  ngOnInit(){
+
+    this.categoriaService.getAll().subscribe({
+      next: categorias => this.categorias = categorias,
+      error: erro => alert("Não foi possivel carregar as categorias")
+    })
+  }
+
+}
