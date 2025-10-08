56fc21926f7b9939c9643da659d594da18887962
unknown <vanderlei123bandeira4@gmail.com>
2025-10-08 17:09:52 -0300

    criando o autor

--- biblioteca-ng/src/app/app.routes.ts
+++ biblioteca-ng/src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { CategoriaList } from './pages/categorias/list/list';
 import { CategoriaCreate } from './pages/categorias/create/create';
 import { CategoriaEdit } from './pages/categorias/edit/edit';
+import { AutorList } from './pages/autores/list/list';
+import { AutorCreate } from './pages/autores/create/create';
 
 export const routes: Routes = [
     {path: "categorias", component: CategoriaList },
     {path: "categorias/cadastrar", component: CategoriaCreate },
     {path: "categorias/editar/:id", component: CategoriaEdit},
+    { path: "autores", component: AutorList },
+    { path: "autores/cadastrar", component: AutorCreate},
 ];
